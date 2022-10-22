0efc93c64917d9ca5cb91be0e1f4d03c78b6c6d8
Guigak <97238914+Guigak@users.noreply.github.com>
2022-10-23 05:10:24 +0900

    fill Chk_with_Enemy function in electric boom class
    fill function

--- play_state.py
+++ play_state.py
@@ -311,7 +311,14 @@ class Electric_Boom:
                             self.position_x, self.position_y)
         pass
 
-    def Chk_with_Enemy(enemy) :
+    def Chk_with_Enemy(self, enemy) :
+        tum_x = enemy.position_x - self.position_x
+        tum_y = enemy.position_y - self.position_y
+
+        tum = math.sqrt(tum_x ** 2 + tum_y ** 2)
+
+        if tum < self.radius + enemy.radius :
+            enemy.alive = False
         pass
 
 # function
