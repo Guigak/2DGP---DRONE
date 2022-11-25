eb627a1ca1a895be7c8748c5dfd01272917cb89c
Guigak <97238914+Guigak@users.noreply.github.com>
2022-11-25 10:26:23 +0900

    fill server
    fill server

---
+++ server.py
@@ -0,0 +1,18 @@
+map = None
+drone = None
+enemies = None
+items = None
+electric_booms = None
+big_drones = None
+mini_drones = None
+electric_balls = None
+shurikens = None
+
+time_create_enemy = None
+time_create_item = None
+time_create_mdrone = None
+
+num_create_mdrone = None
+
+HEIGHT = None
+WIDTH = None
